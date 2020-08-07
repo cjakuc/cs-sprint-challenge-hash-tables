b46f8b376eef509b2b97b796cdfa585fb85680d8
cjakuc <chris.jakuc@gmail.com>
2020-08-07 12:12:23 -0400

    Finished all 5 ex

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,17 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result = []
+    for q in queries:
+        hash_table[q] = ""
+    for f in files:
+        count = f.count("/")
+        f_name = f.split('/', count)
+        f_name = f_name[-1]
+        if f_name in hash_table:
+            result.append(f)
+
 
     return result
 
