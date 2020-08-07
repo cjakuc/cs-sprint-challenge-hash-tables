c8141f0a4469fd26dae5fafe86d9b321d4531614
cjakuc <chris.jakuc@gmail.com>
2020-08-07 11:52:50 -0400

    Finished ex 1-3

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,24 @@ class Ticket:
         self.destination = destination
 
 
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    route = [None] * (length)
+    hash_table = {}
+    # hash_table["NONE"] = tickets[tickets.index()].destination
+    for t in tickets:
+        hash_table[t.source] = t.destination
+    dest = hash_table["NONE"]
+    i = 0
+    while dest != None:
+        route[i] = dest
+        if dest == "NONE":
+            break
+        dest = hash_table[dest]
+        i += 1
 
     return route
